70320e0ffa4d1b732db801ac56e85307e05bc64e
Takayuki Sei <takayuki_sei@voyagegroup.com>
2014-06-03 16:04:20 +0900

    add kiwi templates

---
+++ File Templates/PRH CocoaTouch/Objective-C test case class.xctemplate/XCTestCaseKiwi/___FILEBASENAME___.m
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <XCTest/XCTest.h>
+#import "Kiwi.h"
+
+beforeAll(^{
+});
+afterAll(^{
+});
+
+SPEC_BEGIN(@"<#spec#>")
+describe(@"<#describe#>", ^{
+    context(@"<#context#>", ^{
+        it(@"<#it#>", ^{
+        });
+    });
+});
+SPEC_END
